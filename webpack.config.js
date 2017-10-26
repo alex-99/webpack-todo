27636c6a018156b7ea7649f2feef463305f07e93
Alexander Griffin <alexander.griffin99@hotmail.com>
2017-10-26 01:11:27 -0400

    added es6
    for compiling to prod

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,16 @@ module.exports = {
       }, {
         loader: "sass-loader"
       }]
+    },
+    {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
+        }
+      }
     }]
   }
 };
