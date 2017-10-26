660defb6d16309ac4d2712526e2ee6af8c8763e4
Alexander Griffin <alexander.griffin99@hotmail.com>
2017-10-25 22:49:25 -0400

    added basic webpack shell
    modified package.json and webpack.config.js to work with webpack-dev-server and created a .gitignore file because i forgot to do that

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist"
+  },
+  module: {
+    rules: [{
+      test: /\.sass$/,
+      use: [{
+        loader: "style-loader"
+      }, {
+        loader: "css-loader"
+      }, {
+        loader: "sass-loader"
+      }]
+    }]
+  }
+};
